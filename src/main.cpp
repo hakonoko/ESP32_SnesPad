8d935f8896960f9eebc84f935980b2d078287f34
hakonoko <hakonoko05@gmail.com>
2024-08-16 14:08:48 +0900

    シリアル通信でSFCをPCから操作できるようになった！ SSD1306に押してるボタン表示したいけどどうすればいいかわからん！

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,32 @@
+// Clock Cycle: 1 2 3   4   5 6 7 8 9 10 11 12 13 14 15 16
+// Button     : B Y Sel Sta U D L R A X  L  R  0  0  0  0 
+
+// 1 : 5V    : White
+// 2 : CLOCK : Yellow
+// 3 : LATCH : Orange
+// 4 : DATA  : Red
+// 5 : GND   : Brown
+
 #include <Arduino.h>
 
 #include <Wire.h>               // Only needed for Arduino 1.6.5 and earlier
 #include "SSD1306Wire.h"        // legacy: #include "SSD1306.h"
 
-#define SFC_CLOCK   6
-#define SFC_LATCH   5
+#define GPIO_0to31SET_REG   *((volatile unsigned long *)GPIO_OUT_W1TS_REG)
+#define GPIO_0to31CLR_REG   *((volatile unsigned long *)GPIO_OUT_W1TC_REG)
+
+#define SFC_CLOCK   7
+#define SFC_LATCH   6
 #define SFC_DATA    4
 
 // Initialize the OLED display using Arduino Wire:
-SSD1306Wire display(0x3c, 20, 21);   // ADDRESS, SDA, SCL  -  SDA and SCL usually populate automatically based on your board's pins_arduino.h e.g. https://github.com/esp8266/Arduino/blob/master/variants/nodemcu/pins_arduino.h
+SSD1306Wire display(0x3c, 20, 21);   // ADDRESS, SDA, SCL
 
-int latchCnt = 0;
-bool latch = false;
+volatile bool dataarr[16];
+volatile int bitcounter = 0;
 
-uint16_t buttons  = 0b0000010000000000;
-uint16_t buttons2 = 0b0000100000000000;
-//uint16_t buttons = 0b0010000000000000;
-uint16_t buttonsTemp = 0;
-bool buttonStat = false;
-unsigned long elapsedTime = 0;
+TaskHandle_t drawButtonsTaskHandle = NULL;
+String buttonNames[] = {"B", "Y", "Sel", "Sta", "U", "D", "L", "R", "A", "X", "L", "R", "0", "0", "0", "0" };
 
 void setupPins(){
   pinMode(SFC_LATCH, INPUT);
@@ -28,70 +36,50 @@ void setupPins(){
   Serial.println("pin setup Complete.");
 }
 
-void drawButtons(uint32_t data){
-  Serial.println("Value: " + String(data, HEX));
+// void drawButtons(void *pvParameters){
+//   display.clear();
+//   display.drawString(0, 0, "Hello world");
 
-  display.clear();
-  display.drawString(0, 0, "Hello world");
-  display.drawString(0, 32, "Value: " + String(data, HEX));
-  display.display();
-}
-
-// Clock Cycle: 1 2 3   4   5 6 7 8 9 10 11 12 13 14 15 16
-// Button     : B Y Sel Sta U D L R A X  L  R  0  0  0  0 
-void sendData(){
-  if(!latch){ 
-    // LATCHが立つまで待機
-    if(digitalRead(SFC_LATCH) == HIGH){
-      latch = true;
-    }
-  }else{
-    unsigned long _time = millis();
-    if(_time - elapsedTime > 1000){
-      elapsedTime = millis();
-
-      buttonStat = !buttonStat;
-      if(buttonStat){
-        buttonsTemp = buttons;
-      }else{
-        buttonsTemp = buttons2;
-      }
-    }
-  }
-
-  if(latch){
-    for (int i = 0; i < 16; i++)
-    {
-      // CLOCKがHIGHになるまで待機
-      while(digitalRead(SFC_CLOCK) == LOW){
-        delayMicroseconds(1);
-      }
-      bool push = (buttonsTemp >> (16 - i)) & 0x1;
-      digitalWrite(SFC_DATA, push); // ボタン
-      delayMicroseconds(12);  // 12us待機
-      digitalWrite(SFC_DATA, LOW);  // LOWに変
-    }
-
-    drawButtons(buttonsTemp);
-
-    latch = false;
-  }
-}
-
-// void drawSignal(){
-//     delayMicroseconds(16670);   //16.67ms待機
-//     latchCnt++;
-//     if(latchCnt > 60){
-//       latchCnt = 0;
-//       latch = !latch;
-//       Serial.println("Value: " + String(latch));
-
-//       display.clear();
-//       display.drawString(0, 0, "Hello world");
-//       display.drawString(0, 32, "Value: " + String(latch));
-//       display.display();
-//     }
+//   for(int i=0; i < 16; i++){
+//     display.drawString(30, 15*i, buttonNames[i]);
+//   }
+//   display.display();
 // }
+
+void IRAM_ATTR fastDigitalWrite(bool val, uint8_t pin){
+  if(val){
+    GPIO_0to31SET_REG = 1 << pin;
+    //GPIO.out_w1ts.out_w1ts = 1 << pin;
+  }else{
+    GPIO_0to31CLR_REG = 1 << pin;
+    //GPIO.out_w1tc.out_w1tc = 1 << pin;
+  }
+}
+
+void IRAM_ATTR latching() {
+  bitcounter = 0;
+  fastDigitalWrite(dataarr[bitcounter], SFC_DATA);
+}
+
+void IRAM_ATTR clocking() {
+  if (bitcounter > 15) {
+    fastDigitalWrite(HIGH, SFC_DATA);
+  }else{
+    bitcounter++;
+    fastDigitalWrite(dataarr[bitcounter], SFC_DATA);
+  }
+  
+  // xTaskCreateUniversal(
+  //   drawButtons,
+  //   "DrawButtons",
+  //   8192,
+  //   NULL,
+  //   0,
+  //   &drawButtonsTaskHandle,
+  //   CONFIG_ARDUINO_RUNNING_CORE
+  //   );
+  //drawButtons();
+}
 
 void setup() {
   Serial.begin(115200);
@@ -102,17 +90,46 @@ void setup() {
   // Initialising the UI will init the display too.
   display.init();
 
-  //display.flipScreenVertically();
+  //display.flipScreenVertically();   // ディスプレイを縦に回転
   display.setFont(ArialMT_Plain_10);
 
   display.drawString(0, 0, "Hello world");
   display.display();
 
-  // put your setup code here, to run once:
   setupPins();
+
+  // 
+  attachInterrupt(SFC_LATCH, latching, RISING);
+  attachInterrupt(SFC_CLOCK, clocking, RISING);
+
+  for ( int i = 0; i < 16; i++) {
+    if (i != 4) {
+      dataarr[i] = false;
+    } else {
+      dataarr[i] = true;
+    }
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  sendData();
+  if (!Serial.available()) return;
+  
+  uint8_t buf[2];
+  Serial.readBytes(buf, 2);
+  uint16_t data = (buf[0] << 8) | buf[1];
+
+  Serial.println("ValueBIN: " + String(data, BIN));
+  Serial.println("ValueHEX: " + String(data, HEX));
+
+  for (size_t i = 0; i < 16; i++)
+  {
+    /* code */
+    if ((data >> 15 - i) == 1) {
+      dataarr[i] = false;
+    } else {
+      dataarr[i] = true;
+    }
+  }
+
+  Serial.flush();
 }
