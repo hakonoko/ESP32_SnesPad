03de284f63ba355aef39137a77a0f28b0dced44d
hakonoko <hakonoko05@gmail.com>
2024-08-17 15:58:08 +0900

    i2cは一旦削除

--- src/main.cpp
+++ src/main.cpp
@@ -9,24 +9,16 @@
 
 #include <Arduino.h>
 
-#include <Wire.h>               // Only needed for Arduino 1.6.5 and earlier
-#include "SSD1306Wire.h"        // legacy: #include "SSD1306.h"
-
 #define GPIO_0to31SET_REG   *((volatile unsigned long *)GPIO_OUT_W1TS_REG)
 #define GPIO_0to31CLR_REG   *((volatile unsigned long *)GPIO_OUT_W1TC_REG)
 
 #define SFC_CLOCK   7
 #define SFC_LATCH   6
-#define SFC_DATA    5
-
-// Initialize the OLED display using Arduino Wire:
-SSD1306Wire display(0x3c, 20, 21);   // ADDRESS, SDA, SCL
-
-volatile uint16_t buttons;
-//volatile bool dataarr[16];
-volatile int bitcounter = 0;
+#define SFC_DATA    4
 
 volatile uint16_t buttons = 0;
+//volatile bool dataarr[16];
+volatile int bitcounter = 0;
 
 // TaskHandle_t drawButtonsTaskHandle = NULL;
 // String buttonNames[] = {"B", "Y", "Sel", "Sta", "U", "D", "L", "R", "A", "X", "L", "R", "0", "0", "0", "0" };
@@ -42,16 +34,6 @@ void setupPins(){
   Serial.println("pin setup Complete.");
 }
 
-// void drawButtons(void *pvParameters){
-//   display.clear();
-//   display.drawString(0, 0, "Hello world");
-
-//   for(int i=0; i < 16; i++){
-//     display.drawString(30, 15*i, buttonNames[i]);
-//   }
-//   display.display();
-// }
-
 void IRAM_ATTR fastDigitalWrite(bool val, uint8_t pin){
   if(val){
     GPIO_0to31SET_REG = 1 << pin;
@@ -63,17 +45,13 @@ void IRAM_ATTR fastDigitalWrite(bool val, uint8_t pin){
 }
 
 bool IRAM_ATTR getButton(uint8_t bit){
-  return !(buttons & (1 << bit));
+  return (buttons >> bit) & 1;
 }
 
 void IRAM_ATTR latching() {
   //ulong timer = micros();
   bitcounter = 0;
-  fastDigitalWrite((~buttons & (1 << (16 - bitcounter))), SFC_DATA);  // LOWがボタン押した判定なので反転する
-  //Serial.println(micros() - timer);
-
-  // uint16_t _temp = buttons;
-  // xQueueSendFromISR(xDisplayQueue, &_temp, 0);
+  fastDigitalWrite(!getButton(15 - bitcounter), SFC_DATA);  // LOWがボタン押した判定なので反転する
 }
 
 void IRAM_ATTR clocking() {
@@ -81,7 +59,7 @@ void IRAM_ATTR clocking() {
     fastDigitalWrite(HIGH, SFC_DATA);
   }else{
     bitcounter++;
-    fastDigitalWrite((~buttons & (1 << (16 - bitcounter))), SFC_DATA);  // LOWがボタン押した判定なので反転する
+    fastDigitalWrite(!getButton(15 - bitcounter), SFC_DATA);  // LOWがボタン押した判定なので反転する
   }
 }
 
@@ -90,15 +68,6 @@ void setup() {
   
   Serial.println();
   Serial.println("wake.");
-
-  // Initialising the UI will init the display too.
-  display.init();
-
-  //display.flipScreenVertically();   // ディスプレイを縦に回転
-  display.setFont(ArialMT_Plain_10);
-
-  display.drawString(0, 0, "Hello world");
-  display.display();
 
   setupPins();
 
@@ -109,11 +78,10 @@ void setup() {
   //xTaskCreate(drawButtons, "DrawButtons", 1024, NULL, 0, NULL);
 }
 
-void loop() {
-  if (Serial.available()){
-    uint8_t buf[2];
-    Serial.readBytes(buf, 2);
+uint8_t buf[2];
 
+void loop() {
+  if (Serial.readBytes(buf, 2) != 0){
     buttons = (buf[0] << 8) | buf[1];
 
     Serial.println("ValueBIN: " + String(buttons, BIN));
